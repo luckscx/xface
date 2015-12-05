1e73ef8b4a63e708d0f78455445db82974fbd218
grissomshen(沈陈霄) <grissomshen@tencent.com>
2015-12-05 22:49:26 +0800

    update npm

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -28,33 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
-io.sockets.on('connection',function(socket) {
-    socket.on('new_user', function (data) {
-        socket.name = data.user;
-        if (!users[data.user]) {
-            users[data.user] = data.id;
-        }
-    });
-
-    socket.on('savepic', function (data) {
-        picBiz.savePic(data);
-    });
-
-    socket.on('done', function (data) {
-        var user = data.user;
-        picBiz.getPic(user,function(err,result) {
-            socket.emit('respic',data);
-        });
-    });
-
-    socket.on('disconnect', function() {
-        if (users[socket.name]) {
-            delete users[socket.name];
-        }
-    });
-});
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- redis/redisMgr.js
+++ redis/redisMgr.js
@@ -1,9 +1,8 @@
 /* jshint node:true*/
 "use strict";
 
-var redis_port = 7903;
-
-var redis_client = require('redis').createClient(redis_port,'127.0.0.1');
+var redis_client = require('redis').createClient();
+redis_client.auth('xface@tencent');
 
 
 module.exports = redis_client;
