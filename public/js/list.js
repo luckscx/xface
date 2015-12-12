9aef87a787f5dc8936341635a6349a9644192cf1
aleczhang <aleczhang@tencent.com>
2015-12-13 01:01:50 +0800

    tmp

---
+++ public/js/list.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.img-item img').click(function(){
+        if($(this).parent().find('a').hasClass('cur')) {
+            $(this).parent().find('a').removeClass('cur');
+        } else {
+            $('.cur').removeClass('cur');
+            $(this).parent().find('a').addClass('cur');
+        }
+    })
+    $('.sbm').click(function(){
+        var filename = $('.cur').parent().find('img').attr('src'); 
+        $.ajax({
+            url: 'n/do',
+            type: 'POST',
+            data: {
+                picfile: filename,
+                mailaddress: ''
+            },
+            success: function(json){
+                console.log(json);
+            }
+        }); 
+    })
+    $('.cnl').click(function(){
+        //todo
+    })
+})
