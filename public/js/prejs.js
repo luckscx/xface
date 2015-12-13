edc9e3760bd62c45cb6b68bc549998f5ed9040fa
pancheng <pancheng@tencent.com>
2015-12-13 23:51:10 +0800

    add some views for video present

---
+++ public/js/prejs.js
@@ -0,0 +1,16 @@
+/**
+ * Created by pancheng on 2015/12/13.
+ */
+$(document).ready(function(){
+    $('.preimportsucmine').tap(function(){
+        location.href = '/index.html';
+    });
+
+    $('.preimportsucminec').tap(function(){
+        location.href = '/index.html';
+    });
+
+    $('.preimportsucstar').tap(function(){
+        location.href = '/index.html';
+    });
+});
