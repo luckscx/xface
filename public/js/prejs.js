24cae09923972f7ac732cc7b7fe98c938c2adeb3
pancheng <pancheng@tencent.com>
2015-12-14 00:49:09 +0800

    try to fix bugs

--- public/js/prejs.js
+++ public/js/prejs.js
@@ -1,16 +1,3 @@
 /**
  * Created by pancheng on 2015/12/13.
  */
-$(document).ready(function(){
-    $('.preimportsucmine').tap(function(){
-        location.href = '/index.html';
-    });
-
-    $('.preimportsucminec').tap(function(){
-        location.href = '/index.html';
-    });
-
-    $('.preimportsucstar').tap(function(){
-        location.href = '/index.html';
-    });
-});
