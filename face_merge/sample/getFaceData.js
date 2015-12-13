f77cd81156d6204a0b7cd0504a4a4ae7afdc3629
claudeyang <claudeyang@tencent.com>
2015-12-13 15:13:21 +0800

    detect man face

--- face_merge/sample/getFaceData.js
+++ face_merge/sample/getFaceData.js
@@ -141,8 +141,8 @@ var getFaceData = function(imgPath,out,cb){
     });
 };
 
-var replaceFactor = function(srcFile,dstFile,keyArr,valueArr){
-    var content = fs.readFileSync(srcFile,'utf8');
+var replaceFactor = function(dstFile,keyArr,valueArr){
+    var content = fs.readFileSync('./test_data/src.dat','utf8');
     console.log(content);
     for(var x in keyArr){
         var tmp = content.indexOf(keyArr[x]+'"]=');
@@ -158,7 +158,7 @@ var replaceFactor = function(srcFile,dstFile,keyArr,valueArr){
     fs.writeFileSync(dstFile,content);
 }
 
-var globalFactor = {
+var femaleFactor = {
     'eye_weizhi':100/(11),
     'eye_jianju':(18*100)/(25),
     'eye_kaihe':(28*100)/(10),
@@ -174,7 +174,23 @@ var globalFactor = {
     'gender':1
 };
 
-var minFactor = {
+var maleFactor = {
+    'eye_weizhi':46*1000/(65),
+    'eye_jianju':(25*1000)/(256),
+    'eye_kaihe':(52*1000)/(65),
+    'mouth_daxiao':(38*1000)/(397*2),
+    'mouth_weizhi':(10*1000)/(662),
+    'mouth_xiao':(73*1000)/(270),
+    'nose_kuandu':(110*1000)/(242),
+    'nose_gaodu':(80*1000)/481,
+    'cheek_fengman':(149*1000)/(859),
+    'xiaba_kuandu':(81*1000)/(782*3),
+    'xiaba_weizhi':40,
+    'xiaba_jiankuandu':(100*1000)/(545),
+    'gender':1
+};
+
+var femaleMinFactor = {
     'eye_weizhi':-54,
     'eye_jianju':-17,
     'eye_kaihe':-16,
@@ -190,7 +206,23 @@ var minFactor = {
     'gender':0
 };
 
-var maxFactor = {
+var maleMinFactor = {
+    'eye_weizhi':-46,
+    'eye_jianju':-5,
+    'eye_kaihe':-28,
+    'mouth_daxiao':-27,
+    'mouth_weizhi':-23,
+    'mouth_xiao':-32,
+    'nose_kuandu':-92,
+    'nose_gaodu':-66,
+    'cheek_fengman':-119,
+    'xiaba_kuandu':-36,
+    'xiaba_weizhi':-128,
+    'xiaba_jiankuandu':-105,
+    'gender':0
+};
+
+var femaleMaxFactor = {
     'eye_weizhi':11,
     'eye_jianju':21,
     'eye_kaihe':44,
@@ -206,7 +238,23 @@ var maxFactor = {
     'gender':100
 };
 
-var datFactor = {
+var maleMaxFactor = {
+    'eye_weizhi':15,
+    'eye_jianju':40,
+    'eye_kaihe':32,
+    'mouth_daxiao':55,
+    'mouth_weizhi':13,
+    'mouth_xiao':56,
+    'nose_kuandu':105,
+    'nose_gaodu':33,
+    'cheek_fengman':57,
+    'xiaba_kuandu':126,
+    'xiaba_weizhi':126,
+    'xiaba_jiankuandu':38,
+    'gender':100
+};
+
+var femaleDatFactor = {
     'eye_weizhi':'EYE_POS',
     'eye_jianju':'EYE_DIST',
     'eye_kaihe':'EYE_OPEN',
@@ -222,26 +270,70 @@ var datFactor = {
     'gender':'nRoleType'
 };
 
-var generateDat = function(imgPath,srcDatPath,dstDatPath,cb){
+var maleDatFactor = {
+    'eye_weizhi':'EYE_POS',
+    'eye_jianju':'EYE_DIST',
+    'eye_kaihe':'EYE_OPEN',
+    'mouth_daxiao':'MOUTH_SIZE',
+    'mouth_weizhi':'MOUTH_POS',
+    'mouth_xiao':'MOUTH_END',
+    'nose_kuandu':'NOSE_SIZE',
+    'nose_gaodu':'NOSETOP_POS_Y',
+    'cheek_fengman':'FACE_SCALE',
+    'xiaba_kuandu':'JAW_WIDTH',
+    'xiaba_weizhi':'JAW_POS',
+    'xiaba_jiankuandu':'JAW_END',
+    'gender':'nRoleType'
+};
+
+var maleInfo = {
+    'factor':maleFactor,
+    'minFactor':maleMinFactor,
+    'maxFactor':maleMaxFactor,
+    'datFactor':maleDatFactor,
+};
+
+var femaleInfo = {
+    'factor':femaleFactor,
+    'minFactor':femaleMinFactor,
+    'maxFactor':femaleMaxFactor,
+    'datFactor':femaleDatFactor,
+};
+
+var humanInfo = [maleInfo,femaleInfo];
+
+var generateDat = function(imgPath,dstDatPath,cb){
     var out = {};
     getFaceData(imgPath,out,function(err){
         if(err){
             console.log(err);
         }         
         else{
+            /*
+            var minFactor;
+            var maxFactor;
+            var factor;
+            var datFactor;
+            
+            minFactor = humanInfo[out.gender-1].minFactor;
+            maxFactor = humanInfo[out.gender-1].maxFactor;
+            factor = humanInfo[out.gender-1].factor;
+            datFactor = humanInfo[out.gender-1].datFactor;
             for(var x in out){
-                out[x] = minFactor[x]+Math.round(out[x]*globalFactor[x]);
+                out[x] = minFactor[x]+Math.round(out[x]*factor[x]);
                 out[x] = Math.min(maxFactor[x],out[x]);
                 out[x] = Math.max(minFactor[x],out[x]);
             }
-            replaceFactor(srcDatPath,dstDatPath,datFactor,out);
+            replaceFactor(dstDatPath,datFactor,out);
+            */
+            fs.writeFileSync(dstDatPath,JSON.stringify(out));
         }
         cb(err);
     });
 };
 
 if(require.main === module){
-    generateDat('./test_data/SCUT-FBP-4.jpg','./test_data/src.dat','res.dat',function(err){
+    generateDat('./test_data/man-1.jpg','res.dat',function(err){
         if(err){
             console.log(err);
         }
